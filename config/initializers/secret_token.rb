112c83834c140b1bdb3139e05d7fdced3bcc2c52
Alex <asyrovatsky@gmail.com>
2014-01-20 13:21:13 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllRounds::Application.config.secret_token = '14dc2c4998c25ad33c2e12a5ee1d0421e0f077b96db5bf33b81f0cc148bfdea514fe830b6ebd764c552425f71737ba11c2d14ae2df35aaa99bce67adcb66fdf3'
